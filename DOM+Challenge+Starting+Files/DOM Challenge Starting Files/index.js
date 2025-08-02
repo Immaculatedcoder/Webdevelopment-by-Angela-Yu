93b93646d1578ab27df6e3e2789ba6c3850702eb
Immaculate-07 <adebayoemmanu1111@gmail.com>
2025-08-02 12:34:01 +0100

    Create index.js

---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,7 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Emmanuel";
+
+document.querySelector("a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge"); 
