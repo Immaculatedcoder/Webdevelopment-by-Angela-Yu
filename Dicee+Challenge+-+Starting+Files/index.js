dbd3d4d92f44aa8675d2b3355bb54ec56cae3fff
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-08-19 17:10:02 +0100

    Update index.js

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,3 +1,17 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
-document.firstElementChild.lastElementChild.querySelectorAll("img")
+var value1 = "./images/dice"+randomNumber1 + ".png";
+var value2 = "./images/dice"+randomNumber2 + ".png";
+
+document.firstElementChild.lastElementChild.getElementsByTagName("img")[0].setAttribute("src",value1);
+
+document.firstElementChild.lastElementChild.getElementsByTagName("img")[1].setAttribute("src",value2);
+
+if (randomNumber1 === randomNumber2) {
+    document.firstElementChild.lastElementChild.querySelector("h1").innerHTML = "Draw!";
+} else if(randomNumber1 > randomNumber2) {
+    document.firstElementChild.lastElementChild.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else {
+    document.firstElementChild.lastElementChild.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
