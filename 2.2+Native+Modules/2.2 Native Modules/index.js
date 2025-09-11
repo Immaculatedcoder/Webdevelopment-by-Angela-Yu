18a6a64dc6114b35fb2f918b0bbff41aa1426599
Adebayo Emmanuel <adebayoemmanu1111@gmail.com>
2025-09-11 14:23:01 -0400

    Update index.js

--- 2.2+Native+Modules/2.2 Native Modules/index.js
+++ 2.2+Native+Modules/2.2 Native Modules/index.js
@@ -1,1 +1,12 @@
-console.log("This is emmanuel")
+
+const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello Node", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+
+fs.readFile("message.txt", 'utf-8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
