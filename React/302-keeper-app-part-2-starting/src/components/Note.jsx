3d8344428002513b34f7d8d3118c911846fc13fe
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-09 23:51:26 -0500

    add

--- React/302-keeper-app-part-2-starting/src/components/Note.jsx
+++ React/302-keeper-app-part-2-starting/src/components/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import notes from "../notes";
+
+function generateNote(note) {
+  return (
+  <div className="note" key={note.key}>
+    <h1> {note.title}</h1>
+    <p> {note.content}</p>
+  </div> );
+}
 
 function Note() {
   return (
-    <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
-    </div>
+    notes.map(generateNote)
   );
 }
 
