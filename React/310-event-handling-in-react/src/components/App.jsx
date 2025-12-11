779981e5c25eabef956d51033226ac683ace3aca
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-11 00:49:07 -0500

    aa

--- React/310-event-handling-in-react/src/components/App.jsx
+++ React/310-event-handling-in-react/src/components/App.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [headingText, setHeadingText] = useState("Hello")
+  function handleClick() {
+    setHeadingText("Submitted")
+  }
+
+  const [color, setColor] = useState("white")
+
+  function customColorOver(){
+    setColor("black")
+  }
+
+  function customColorOut() {
+    setColor("white")
+  }
+
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick} onMouseOver={customColorOver} onMouseOut={customColorOut} style={{backgroundColor:color}}>Submit</button>
     </div>
   );
 }
 
 export default App;
+ 
