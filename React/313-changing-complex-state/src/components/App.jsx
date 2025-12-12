db34f6d9ccbf12eaaefdd9f61aab2f4e6f2eca3f
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-11 22:52:35 -0500

    Add

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,52 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  // const [fname, setfname] = useState("");
+  // const [lname, setlname] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  // function fnameChange(event) {
+  //   setfname(event.target.value)
+  // }
+  // function lnameChange(event) {
+  //   setlname(event.target.value)
+  // }
+
+  function handleChange(event) { 
+    // const newValue = event.target.value;
+    // const inputName = event.target.name;
+
+    const {value, name} = event.target
+
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName : value,
+          lName : prevValue.lName
+        }
+      } else if  (name === "lName") {
+        return {
+          fName : prevValue.fName,
+          lName : value
+        }
+      }
+    })
+    
+
+    // const [value, name] = event.target
+    // console.log(value)
+    // console.log(name)
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="fName" placeholder="First Name" value={fullName.fName}/>
+        <input onChange={handleChange} name="lName" placeholder="Last Name" value={fullName.lName} />
         <button>Submit</button>
       </form>
     </div>
