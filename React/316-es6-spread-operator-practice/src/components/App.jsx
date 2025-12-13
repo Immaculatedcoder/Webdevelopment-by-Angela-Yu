24bff3f5c2ac8ae7c72cb03c53cfe40b8878e74a
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-13 00:24:54 -0500

    mm

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  // const [itemK, setItemK] = useState("");
+
+  const [itemC, setItemC] = useState("");
+
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    setItemC(event.target.value)
+  }
+
+  function handleClick() {
+    setItems(preItems => {return  [...preItems, itemC]});
+    setItemC("");    
+  }
+
+  
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={itemC}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item, index) => {return (
+            <li key={index}>{item}</li>
+          );} )}
+          {/* <li>{itemK}</li> */}
         </ul>
       </div>
     </div>
