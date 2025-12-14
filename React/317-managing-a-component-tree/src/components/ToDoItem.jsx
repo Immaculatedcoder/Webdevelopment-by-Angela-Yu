1bf38cc8213cd88bde70ffe3cfd0ac0b3e3f3b32
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-13 19:08:42 -0500

    Good to go

--- React/317-managing-a-component-tree/src/components/ToDoItem.jsx
+++ React/317-managing-a-component-tree/src/components/ToDoItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 function ToDoItem(props) {
-    return <li> {props.text}</li>
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClicked() {
+        setIsDone(prev => {
+            return !prev;
+        })
+    }
+
+    // return <li onClick={handleClicked} style={{textDecoration: isDone ? "line-through" : "none"}}> {props.text}</li>
+    return <li onClick={() => {props.onChecked(props.id)}} style={{textDecoration: isDone ? "line-through" : "none"}}> {props.text}</li>
+    // return <li onClick={props.onChecked(props.id)} style={{textDecoration: isDone ? "line-through" : "none"}}> {props.text}</li>
 }
 
 export default ToDoItem
